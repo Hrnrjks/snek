44b73e481722dd9c982e4f1fedd87eb797bc5b0a
zellar <empty@email>
2019-08-25 21:05:54 +0300

    done

---
+++ snek/snek/Input.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snek
+{
+    class Input
+    {
+        // load available keyboard keys
+        private static Hashtable keyTable = new Hashtable();
+
+        // check whether a specific 'key' is pressed
+        public static bool KeyPressed(Keys key)
+        {
+            if (keyTable[key] == null)
+            {
+                return false;
+            }
+
+            return (bool)keyTable[key];
+        }
+
+        // if the 'key''s state is changed then update the state in the keyTable
+        public static void ChangeState(Keys key, bool state)
+        {
+            keyTable[key] = state;
+        }
+    }
+}
